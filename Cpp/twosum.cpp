38a0e88e6d8c23cbddc434dfc7589d8d672d116c
Permofrost <danysh.3197@gmail.com>
2024-03-16 16:15:49 -0400

    Pushing what's done so far

---
+++ Cpp/twosum.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> map;
+        vector<int> result;
+
+        for(int i = 0; i < nums.size(); i++){
+            int complement = target - nums[i];
+            if(map.find(complement) != map.end()){
+                result.push_back(map[complement]);
+                result.push_back(i);
+                return result;
+            }
+            map[nums[i]] = i;
+        }
+        return result;
+    }
+};
+
+
+int main() {
+    Solution solution;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = solution.twoSum(nums, target);
+    for(int i : result) {
+        cout << i << " ";
+    }
+    return 0;
+}
+
+
--- Cpp/vanagram.cpp
+++ Cpp/vanagram.cpp
@@ -7,9 +7,23 @@ using namespace std;
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
         
+        unordered_map<char,int> smap;
+        unordered_map<char,int> tmap;
+        
+        for(int i = 0; i < s.size(); i++){
+            smap[s[i]]++;
+            tmap[t[i]]++;
+        }
+        
+        for(auto &kv : smap){
+            if(kv.second != tmap[kv.first]) return false;
+        }
+        return true;
     }
-};
+}; 
+
 
 int main() {
     Solution solution;
@@ -19,3 +33,5 @@ int main() {
     cout << (result ? "true" : "false") << endl;
     return 0;
 }
+
+
